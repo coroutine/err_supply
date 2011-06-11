72ebee5d428a6f598dcd71e1a68e79d779878420
John Dugan <jdugan@coroutine.com>
2011-06-10 21:05:05 -0500

    Trying to get generator to show up in rails properly.

---
+++ generators/err_supply/install/install_generator.rb
@@ -0,0 +1,12 @@
+module ErrSupply
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
+
+      def add_javascript_files
+        copy_file "jquery.qtip-1.0.0-rc3.min.js", "public/javascripts/jquery.qtip-1.0.0-rc3.min.js"
+        copy_file "err_supply.js",                "public/javascripts/err_supply.js"
+      end
+    end
+  end
+end
