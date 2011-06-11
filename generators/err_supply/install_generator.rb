3560c07e2b8d886abc7d9dbb9ed4fb26c9dd9fcb
John Dugan <jdugan@coroutine.com>
2011-06-10 20:53:07 -0500

    Added generator usage file.

--- generators/err_supply/install_generator.rb
+++ generators/err_supply/install_generator.rb
@@ -1,12 +1,10 @@
 module ErrSupply
-  module Generators
-    class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('../templates', __FILE__)
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
 
-      def generate_javascript
-        copy_file "jquery.qtip-1.0.0-rc3.min.js", "public/javascripts/jquery.qtip-1.0.0-rc3.min.js"
-        copy_file "err_supply.js",                "public/javascripts/err_supply.js"
-      end
+    def generate_javascript
+      copy_file "jquery.qtip-1.0.0-rc3.min.js", "public/javascripts/jquery.qtip-1.0.0-rc3.min.js"
+      copy_file "err_supply.js",                "public/javascripts/err_supply.js"
     end
   end
 end
