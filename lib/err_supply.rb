dd4581afcb22e96415166b77d8e199c8088c5d9b
John Dugan <jdugan@coroutine.com>
2012-12-19 11:11:35 -0600

    Changed mixin of view helper. Bumped to 0.1.4.

--- lib/err_supply.rb
+++ lib/err_supply.rb
@@ -3,6 +3,16 @@ require 'err_supply/view_helpers'
 
 module ErrSupply
   class Engine < ::Rails::Engine
-    ActionView::Base.module_eval { include ErrSupply::ViewHelpers }  if defined? ActionView
+    
+    #-------------------------------------------------------
+    # Initialization
+    #-------------------------------------------------------
+    initializer "err_supply.start" do |app|
+      
+      # add method to views
+      ActionView::Base.module_eval { include ErrSupply::ViewHelpers }  if defined? ActionView
+      
+    end
+    
   end
 end
